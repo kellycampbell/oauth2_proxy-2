10d44be6e06a2679c613056c5b81b9535e12621e
Kelly Campbell <kellyc@stratisiot.com>
2019-01-23 12:30:39 -0500

    Apply ssl-insecure-skip-verify option to upstream servers
    2019/01/23 17:26:59 reverseproxy.go:395: http: proxy error: x509: certificate is valid for us-east-2.foo.com, not kubernetes-dashboard

--- options.go
+++ options.go
@@ -156,6 +156,7 @@ func (o *Options) Validate() error {
 			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 		}
 		http.DefaultClient = &http.Client{Transport: insecureTransport}
+		http.DefaultTransport = insecureTransport
 	}
 
 	msgs := make([]string, 0)
